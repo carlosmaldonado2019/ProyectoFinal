1b00eb3c7e21b989c01f662999e6703084401b96
Maldy08 <karloz_0986@hotmail.com>
2019-04-23 21:40:14 -0700

    fragment

---
+++ app/src/main/java/com/mgtic/dapm/proyectofinal/MyFragmentPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.mgtic.dapm.proyectofinal
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MyFragmentPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
